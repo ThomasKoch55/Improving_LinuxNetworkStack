84c76045c655c7d10abb2acf3f9d519a2ea759a9
twabulldogg <twabulldogg>
2023-05-15 14:45:41 +0000

    fixed typo

--- xdp_bcc_way/compile.py
+++ xdp_bcc_way/compile.py
@@ -7,11 +7,11 @@ cflgs = ['-O2', '-std=gnu89', '-w', '-D__KERNEL__', '-D__TARGET_ARCH_x86']
 def attach(iface):
     b = BPF(src_file="std_trie_kern.c", cflags=cflgs)
 
-    func = b.load_func("xdp_prog_simple", BPF.XDP, device="iface")
+    func = b.load_func("xdp_prog_simple", BPF.XDP)
 
 
     
-    b.attach_xdp(dev=iface, fn=func, flags=0)#, flags=XDP_FLAGS_DRV_MODE)
+    b.attach_xdp(iface, func, 0)#, flags=XDP_FLAGS_DRV_MODE)
 
 #def detach():
 #    BPF.remove_xdp(dev="ens33")
